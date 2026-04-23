535e5442487ad6c5c0f020abd3a23d2fe2362c70
2019147588 <chini2892@naver.com>
2026-04-23 18:08:23 +0900

    feat: 추가 문제 풀이

--- 01_algorithms/04_week/kyh/p5_1322/Main.java
+++ 01_algorithms/04_week/kyh/p5_1322/Main.java
@@ -4,10 +4,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.Buffer;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long X = Long.parseLong(st.nextToken());
+        long K = Long.parseLong(st.nextToken());
+
+        long Y = 0L;
+        int count = 0;
+        while(K>0){
+            while((X&(1L << count))!= 0) count++;
+            if((K&1)==1) Y |= (1L<<count);
+
+            K >>= 1;
+            count++;
+
+        }
+        System.out.println(Y);
+
 
     }
 }
--- 01_algorithms/04_week/kyh/p7_2098/Main.java
+++ 01_algorithms/04_week/kyh/p7_2098/Main.java
@@ -1,6 +1,61 @@
 package kyh.p7_2098;
-import java.io.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
+    static int[][] cost;
+    static int[][] dp;
+    static final int INF = 1000000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        cost = new int[N][N];
+        dp = new int[N][1 << N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                cost[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < (1 << N); j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println(tsp(0, 1));
+    }
+
+    static int tsp(int now, int visited) {
+        if (visited == (1 << N) - 1) {
+            if (cost[now][0] == 0) return INF;
+            return cost[now][0];
+        }
+
+        if (dp[now][visited] != -1) {
+            return dp[now][visited];
+        }
+
+        dp[now][visited] = INF;
+
+        for (int next = 0; next < N; next++) {
+            if ((visited & (1 << next)) != 0 || cost[now][next] == 0) {
+                continue;
+            }
+
+            int newVisited = visited | (1 << next);
+            dp[now][visited] = Math.min(dp[now][visited],
+                    cost[now][next] + tsp(next, newVisited));
+        }
+
+        return dp[now][visited];
+    }
 }
